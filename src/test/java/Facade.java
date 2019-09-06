f9760ba84d179f26f204875e7c2436f03d6fe67e
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-09-06 10:31:16 +0200

    something

---
+++ src/test/java/Facade.java
@@ -0,0 +1,74 @@
+
+import utils.EMF_Creator;
+import entities.Movie;
+import facades.MovieFacade;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utils.Settings;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+public class Facade {
+
+    private static EntityManagerFactory emf;
+    private static MovieFacade facade;
+
+    //@BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactory(
+                "pu",
+                "jdbc:mysql://localhost:3307/startcode_test",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
+        facade = MovieFacade.getMovieFacade(emf);
+        String s = "build plx";
+    }
+    @BeforeAll
+    public static void setUpClassV2() {
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.DROP_AND_CREATE);
+        facade = MovieFacade.getMovieFacade(emf);
+    }
+
+    public static void tearDownClass() {
+//        Clean up database after test is done or use a persistence unit with drop-and-create to start up clean on every test
+    }
+
+    // Setup the DataBase in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the script below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
+            em.persist(new Movie(2, "ree", "res"));
+            em.persist(new Movie(1, "ree", "res"));
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+//        Remove any data after each test was run
+    }
+
+    // TODO: Delete or change this method
+    @Disabled
+    @Test
+    public void testAFacadeMethod() {
+        assertEquals(2, facade.getMovieCount(), "Expects two rows in the database");
+    }
+
+
+}
